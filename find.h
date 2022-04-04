9c37d82e93b3ed1dd6f62b4fdf01dc01165c231a
Puliedro <2009048@upy.edu.mx>
2022-04-03 23:52:02 -0500

    Added find.h

--- find.h
+++ find.h
@@ -1,5 +1,16 @@
-int search(int arr[], int length)
+int search(int arr[], int length, int num)
 {
-  int queso = 0;
-  return queso;
+  int i = 0;
+  int hasthenumberbeenfound = 0;
+  for (int i = 0; i < length; i++){
+      if(arr[i] == num){
+          printf("The value %d has been found in the array, in the position %d!", num, i);
+          hasthenumberbeenfound = 1;
+          break;
+      }
+  }
+  if (hasthenumberbeenfound == 0){
+      printf("Sorry, the value %d has not been found in the array", num, i);
+  }
+  return 0;
 }
