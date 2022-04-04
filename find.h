5e2e578fbd280aee69fbc18ae99819e8537cc9af
Puliedro <2009048@upy.edu.mx>
2022-04-04 00:21:14 -0500

    Fixed find.h

--- find.h
+++ find.h
@@ -1,16 +1,12 @@
 int search(int arr[], int length, int num)
 {
   int i = 0;
-  int hasthenumberbeenfound = 0;
+  int back = 0;
   for (int i = 0; i < length; i++){
       if(arr[i] == num){
-          printf("The value %d has been found in the array, in the position %d!", num, i);
-          hasthenumberbeenfound = 1;
+          back = i;
           break;
       }
   }
-  if (hasthenumberbeenfound == 0){
-      printf("Sorry, the value %d has not been found in the array", num, i);
-  }
-  return 0;
+  return back;
 }
