e7b5611cca9858f35a958515307666c4ea8a3f6a
Juan Fernandez <2109061@upy.edu.mx>
2022-04-04 00:51:30 -0500

    finished

--- main.c
+++ main.c
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include "./max.h"
 #include "./min.h"
+#include "./find.h"
 
 int main(){
     int array[] = {23, 3564, 32, 345};
     int len = sizeof(array)/ sizeof(int);
     int max = maximo(array, len);
     int min = mini(array, len);
+    int num= 0;
+
     printf("MAX:\t%d\n", max);
     printf("MIN:\t%d\n", min);
+    printf("TYPE THE NUMBER: \n");
+    scanf("%d", &num);
+    int find = search(array, len, num);
+    if (find == -1){
+        printf("THE NUMBER IS NOT IN THE ARRAY\n");
+
+    }
+    else{
+        printf("THE NUMER IS AT POSITION: %d \n", find);
+    }
 
     return 0 ;
 }
